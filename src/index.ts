837dbade0dbdd27e4d5a9d079f604d2139681051
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-10-16 23:46:54 +0530

    added edge case

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,72 @@ export default function transform(file, api, options) {
   const root = j(file.source);
   let dirtyFlag = false;
 
-  // Replace `use` property with `url-loader` or `loader` property with `url-loader` to `type: 'asset/inline'`
-  root.find(j.ObjectExpression).forEach(path => {
-    const usePropertyIndex = path.node.properties.findIndex(prop =>
-      j.ObjectProperty.check(prop) &&
-      j.Identifier.check(prop.key) &&
-      prop.key.name === 'use' &&
-      ((j.ArrayExpression.check(prop.value) && prop.value.elements.some(el => j.StringLiteral.check(el) && el.value === 'url-loader')) ||
-        (j.ObjectExpression.check(prop.value) && prop.value.properties.some(innerProp =>
-          j.ObjectProperty.check(innerProp) &&
-          j.Identifier.check(innerProp.key) &&
-          innerProp.key.name === 'loader' &&
-          j.StringLiteral.check(innerProp.value) &&
-          innerProp.value.value === 'url-loader')))
-    );
-
-    if (usePropertyIndex !== -1) {
-      path.node.properties[usePropertyIndex] = j.objectProperty(j.identifier('type'), j.stringLiteral('asset/inline'));
-      dirtyFlag = true;
-    }
+  // Replace `use` or `loader` property with `url-loader` to `type: 'asset/inline'`
+  root.find(j.ObjectExpression).forEach((path) => {
+      path.node.properties.forEach((prop, index) => {
+          if (j.ObjectProperty.check(prop) && j.Identifier.check(prop.key)) {
+              // Check for `use` as an array
+              if (
+                  prop.key.name === 'use' &&
+                  j.ArrayExpression.check(prop.value) &&
+                  prop.value.elements.some(
+                      (el) =>
+                          j.StringLiteral.check(el) &&
+                          el.value === 'url-loader',
+                  )
+              ) {
+                  path.node.properties[index] = j.objectProperty(
+                      j.identifier('type'),
+                      j.stringLiteral('asset/inline'),
+                  );
+                  dirtyFlag = true;
+              }
+              // Check for `use` as an object with `loader`
+              else if (
+                  prop.key.name === 'use' &&
+                  j.ObjectExpression.check(prop.value) &&
+                  prop.value.properties.some(
+                      (innerProp) =>
+                          j.ObjectProperty.check(innerProp) &&
+                          j.Identifier.check(innerProp.key) &&
+                          innerProp.key.name === 'loader' &&
+                          j.StringLiteral.check(innerProp.value) &&
+                          innerProp.value.value === 'url-loader',
+                  )
+              ) {
+                  path.node.properties[index] = j.objectProperty(
+                      j.identifier('type'),
+                      j.stringLiteral('asset/inline'),
+                  );
+                  dirtyFlag = true;
+              }
+              // Check for `loader` directly
+              else if (
+                  prop.key.name === 'loader' &&
+                  j.StringLiteral.check(prop.value) &&
+                  prop.value.value === 'url-loader'
+              ) {
+                  path.node.properties[index] = j.objectProperty(
+                      j.identifier('type'),
+                      j.stringLiteral('asset/inline'),
+                  );
+                  dirtyFlag = true;
+              }
+          }
+      });
   });
 
   // Rename function `webpack` to `rspack`
-  root.find(j.ObjectProperty, { key: { name: 'webpack' } }).forEach(path => {
-    if (j.Identifier.check(path.node.key)) {
-      path.node.key.name = 'rspack';
-      dirtyFlag = true;
-    }
-  });
+  root.find(j.ObjectProperty, { key: { name: 'webpack' } }).forEach(
+      (path) => {
+          if (j.Identifier.check(path.node.key)) {
+              path.node.key.name = 'rspack';
+              dirtyFlag = true;
+          }
+      },
+  );
 
   return dirtyFlag ? root.toSource() : undefined;
 }
 
-
-export const parser = "tsx";
+export const parser = 'tsx';
